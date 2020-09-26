4fcf2c22a0308a72d1dd77eaff18c0b76f29dc6e
Oluwatobi Ajide <biajide@gmail.com>
2020-09-25 22:12:03 -0400

    template

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(`${__dirname}/public`));
+
+
+app.listen(port, () => {
+    console.log(`Node app is running at localhost:${port}`);
+  });
